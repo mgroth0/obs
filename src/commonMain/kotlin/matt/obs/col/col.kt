1a7c605d2ee7861ecd02fe037691a86237893634
mgroth0 <mgroth49@gmail.com>
2022-09-10 19:52:36 -0400

    autocommit

--- src/commonMain/kotlin/matt/obs/col/col.kt
+++ src/commonMain/kotlin/matt/obs/col/col.kt
@@ -21,35 +21,59 @@ sealed interface CollectionChange<E> {
   val collection: Collection<E>
 }
 
-sealed class Addition<E>(override val collection: Collection<E>, val added: E): CollectionChange<E>
+
+sealed interface AdditionBase<E>: CollectionChange<E> {
+  val addedElements: List<E>
+}
+
+sealed interface RemovalBase<E>: CollectionChange<E> {
+  val removedElements: List<E>
+}
+
+sealed class Addition<E>(override val collection: Collection<E>, val added: E): AdditionBase<E> {
+  override val addedElements get() = listOf(added)
+}
+
 class AddAtEnd<E>(collection: Collection<E>, added: E): Addition<E>(collection, added)
-
 class AddAt<E>(collection: Collection<E>, added: E, val index: Int): Addition<E>(collection, added)
+sealed class MultiAddition<E>(override val collection: Collection<E>, val added: Collection<E>): AdditionBase<E> {
+  override val addedElements get() = added.toList()
+}
 
-sealed class MultiAddition<E>(override val collection: Collection<E>, val added: Collection<E>): CollectionChange<E>
 class MultiAddAtEnd<E>(collection: Collection<E>, added: Collection<E>): MultiAddition<E>(collection, added)
-
 class MultiAddAt<E>(collection: Collection<E>, added: Collection<E>, val index: Int):
   MultiAddition<E>(collection, added)
 
-sealed class Removal<E>(override val collection: Collection<E>, val removed: E): CollectionChange<E>
+
+sealed class Removal<E>(override val collection: Collection<E>, val removed: E): RemovalBase<E> {
+  override val removedElements get() = listOf(removed)
+}
+
 class RemoveElement<E>(collection: Collection<E>, removed: E): Removal<E>(collection, removed)
 class RemoveAt<E>(collection: Collection<E>, removed: E, val index: Int): Removal<E>(collection, removed)
 class RemoveFirst<E>(collection: Collection<E>, removed: E): Removal<E>(collection, removed)
 
-sealed class MultiRemoval<E>(override val collection: Collection<E>, val removed: Collection<E>): CollectionChange<E>
-class RemoveElements<E>(collection: Collection<E>, removed: Collection<E>): MultiRemoval<E>(collection, removed)
+sealed class MultiRemoval<E>(override val collection: Collection<E>, val removed: Collection<E>): RemovalBase<E> {
+  override val removedElements get() = removed.toList()
+}
 
+class RemoveElements<E>(collection: Collection<E>, removed: Collection<E>): MultiRemoval<E>(collection, removed)
 class RetainAll<E>(collection: Collection<E>, removed: Collection<E>, val retained: Collection<E>):
   MultiRemoval<E>(collection, removed)
 
-sealed class Replacement<E>(override val collection: Collection<E>, val removed: E, val added: E): CollectionChange<E>
+
+sealed class Replacement<E>(override val collection: Collection<E>, val removed: E, val added: E): AdditionBase<E>,
+																								   RemovalBase<E> {
+  override val addedElements get() = listOf(added)
+  override val removedElements get() = listOf(removed)
+}
+
 class ReplaceElement<E>(collection: Collection<E>, removed: E, added: E): Replacement<E>(collection, removed, added)
 
 class ReplaceAt<E>(collection: Collection<E>, removed: E, added: E, val index: Int):
   Replacement<E>(collection, removed, added)
 
-class Clear<E>(override val collection: Collection<E>): CollectionChange<E>
+class Clear<E>(collection: Collection<E>, removed: Collection<E>): MultiRemoval<E>(collection, removed)
 
 
 abstract class BasicROObservableCollection<E>: MObservableWithChangeObjectImpl<CollectionChange<E>>(), Collection<E> {
