1caceaa98cc8d1eba837bad06cba9fc19de506f5
mgroth0 <mgroth49@gmail.com>
2022-10-05 16:42:54 -0400

    autocommit

--- math/src/commonMain/kotlin/matt/obs/math/ranges/ranges.kt
+++ math/src/commonMain/kotlin/matt/obs/math/ranges/ranges.kt
@@ -8,7 +8,7 @@ operator fun ObsI.rangeTo(other: ObsI): Sequence<ObsI> = value.rangeTo(other.val
 
 operator fun ObsI.rangeTo(other: Int): Sequence<ObsI> = value.rangeTo(other).asSequence().map(::BindableProperty)
 
-operator fun ObsI.rangeTo(other: ObsL): Sequence<ObsL> = value.rangeTo(other.value).asSequence().map(::BindableProperty)
+/*operator fun ObsI.rangeTo(other: ObsL): Sequence<ObsL> = value.rangeTo(other.value).asSequence().map(::BindableProperty)*/
 
 operator fun ObsI.rangeTo(other: Long): Sequence<ObsL> = value.rangeTo(other).asSequence().map(::BindableProperty)
 
@@ -17,6 +17,6 @@ operator fun ObsL.rangeTo(other: ObsL): Sequence<ObsL> = value.rangeTo(other.val
 
 operator fun ObsL.rangeTo(other: Long): Sequence<ObsL> = value.rangeTo(other).asSequence().map(::BindableProperty)
 
-operator fun ObsL.rangeTo(other: ObsI): Sequence<ObsL> = value.rangeTo(other.value).asSequence().map(::BindableProperty)
+/*operator fun ObsL.rangeTo(other: ObsI): Sequence<ObsL> = value.rangeTo(other.value).asSequence().map(::BindableProperty)*/
 
 operator fun ObsL.rangeTo(other: Int): Sequence<ObsL> = value.rangeTo(other).asSequence().map(::BindableProperty)
