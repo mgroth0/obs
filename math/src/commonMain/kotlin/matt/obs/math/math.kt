7b6f042f1769965a52df5573cfde23b57b1bc81d
mgroth0 <mgroth49@gmail.com>
2022-10-05 17:38:04 -0400

    autocommit

--- math/src/commonMain/kotlin/matt/obs/math/math.kt
+++ math/src/commonMain/kotlin/matt/obs/math/math.kt
@@ -2,20 +2,13 @@ package matt.obs.math
 
 import matt.lang.Num
 import matt.math.neg.unaryMinus
-import matt.math.op.div
-import matt.math.op.minus
-import matt.math.op.plus
-import matt.math.op.rem
-import matt.math.op.times
 import matt.obs.bind.binding
 import matt.obs.prop.ObsVal
-import matt.obs.prop.Var
 
 typealias ObsNum = ObsVal<out Num>
 
 /*operator fun <N: Number> ObsVal<N>.unaryPlus(): ObsVal<N> = binding { it }*/
 operator fun <N: Number> ObsVal<N>.unaryMinus(): ObsVal<N> = binding { -it }.cast()
-
 
 
 fun <N: Num> reduction(vararg values: ObsVal<N>, op: (Array<out ObsVal<N>>)->N): ObsVal<N> {
@@ -27,5 +20,8 @@ fun <N: Num> reduction(vararg values: ObsVal<N>, op: (Array<out ObsVal<N>>)->N):
 }
 
 
-
-
+fun <N: Number> ObsNum.asInt() = binding { it.toInt() }
+fun <N: Number> ObsNum.asLong() = binding { it.toLong() }
+fun <N: Number> ObsNum.asShort() = binding { it.toShort() }
+fun <N: Number> ObsNum.asFloat() = binding { it.toFloat() }
+fun <N: Number> ObsNum.asDouble() = binding { it.toDouble() }
