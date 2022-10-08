f38040f7f0fb620ce975f109d719f597645f3894
mgroth0 <mgroth49@gmail.com>
2022-10-07 21:23:06 -0400

    autocommit

---
+++ src/jvmMain/kotlin/matt/obs/watch/watch.kt
@@ -0,0 +1,19 @@
+package matt.obs.watch
+
+import matt.obs.prop.BindableProperty
+import matt.obs.prop.ObsVal
+import matt.time.dur.sleep
+import kotlin.concurrent.thread
+import kotlin.time.Duration
+
+
+fun <T> watchProp(checkInterval: Duration, op: ()->T): ObsVal<T> {
+  val prop = BindableProperty(op())
+  thread(isDaemon = true) {
+	while (true) {
+	  prop.value = op()
+	  sleep(checkInterval)
+	}
+  }
+  return prop
+}
