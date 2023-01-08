eb5f45f059f86cd75e3ef0db71a14eb980772d41
mgroth0 <mgroth49@gmail.com>
2023-01-08 13:11:09 -0500

    autocommit

--- src/commonMain/kotlin/matt/obs/prop/prop.kt
+++ src/commonMain/kotlin/matt/obs/prop/prop.kt
@@ -53,7 +53,7 @@ value class FakeObsVal<T>(override val value: T): MObservableValNewAndOld<T> {
 
   override var debugger: Prints?
 	get() = TODO("Not yet implemented")
-	set(value) {
+	set(_) {
 	  TODO("Not yet implemented")
 	}
 
