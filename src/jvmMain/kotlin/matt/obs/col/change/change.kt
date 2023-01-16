85db9f226329589d01cdcb0e38fed31bea8eab71
mgroth0 <mgroth49@gmail.com>
2023-01-16 11:46:22 -0500

    autocommit

--- src/jvmMain/kotlin/matt/obs/col/change/change.kt
+++ src/jvmMain/kotlin/matt/obs/col/change/change.kt
@@ -6,13 +6,13 @@ sealed interface QueueChange<E>: CollectionChange<E, Queue<E>>
 
 
 class QueueAdd<E>(val added: E, override val collection: Queue<E>): QueueChange<E> {
-  override fun <T> convert(collection: Collection<T>, convert: (E)->T): CollectionChange<T, out Collection<T>> {
+  override fun <T> convert(collection: Collection<T>, convert: (E)->T): QueueAdd<T> {
 	return QueueAdd(convert(added), collection as Queue<T>)
   }
 }
 
 class QueueRemove<E>(val removed: E, override val collection: Queue<E>): QueueChange<E> {
-  override fun <T> convert(collection: Collection<T>, convert: (E)->T): CollectionChange<T, out Collection<T>> {
+  override fun <T> convert(collection: Collection<T>, convert: (E)->T): QueueRemove<T> {
 	return QueueRemove(convert(removed), collection as Queue<T>)
   }
 }
