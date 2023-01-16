b7fe98044365361657ffb9ee07f288ae391d81db
mgroth0 <mgroth49@gmail.com>
2023-01-16 17:13:46 -0500

    autocommit

--- src/commonMain/kotlin/matt/obs/col/olist/mappedlist/MappedList.kt
+++ src/commonMain/kotlin/matt/obs/col/olist/mappedlist/MappedList.kt
@@ -3,6 +3,7 @@ package matt.obs.col.olist.mappedlist
 
 import matt.lang.model.value.LazyValue
 import matt.lang.setAll
+import matt.lang.setall.setAll
 import matt.lang.weak.WeakRef
 import matt.obs.col.change.mirror
 import matt.obs.col.olist.BasicObservableListImpl
