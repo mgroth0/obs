82b363b237c40cff92ffeabcf47ede78d06475c1
mgroth0 <mgroth49@gmail.com>
2023-02-05 21:51:12 -0500

    autocommit

--- src/commonMain/kotlin/matt/obs/prop/typed/typed.kt
+++ src/commonMain/kotlin/matt/obs/prop/typed/typed.kt
@@ -2,13 +2,18 @@
 
 package matt.obs.prop.typed
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.CompositeEncoder
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.serializer
 import matt.obs.prop.BindableProperty
 import matt.reflect.classForName
 import kotlin.reflect.KClass
@@ -53,5 +58,27 @@ class TypedBindablePropertySerializer<T>(private val dataSerializer: KSerializer
 
 inline fun <reified T> typedBindableProperty(value: T) = TypedBindableProperty(T::class, null is T, value)
 
+@OptIn(InternalSerializationApi::class)
 @Serializable(with = TypedBindablePropertySerializer::class)
-class TypedBindableProperty<T>(val cls: KClass<*>, val nullable: Boolean, value: T): BindableProperty<T>(value)
+class TypedBindableProperty<T>(val cls: KClass<*>, val nullable: Boolean, value: T): BindableProperty<T>(value) {
+  fun encode(encoder: CompositeEncoder, index: Int) {
+	val ser = cls.serializer()
+	@Suppress("UNCHECKED_CAST")
+	encoder.encodeSerializableElement<T>(
+	  descriptor = ser.descriptor,
+	  index = index,
+	  serializer = ser as SerializationStrategy<T>,
+	  value = value
+	)
+  }
+
+  fun decode(decoder: CompositeDecoder, index: Int) {
+	val ser = cls.serializer()
+	@Suppress("UNCHECKED_CAST") val loadedValue = decoder.decodeSerializableElement<T>(
+	  descriptor = ser.descriptor,
+	  index = index,
+	  deserializer = ser as DeserializationStrategy<T>
+	)
+	value = loadedValue
+  }
+}
--- src/jvmMain/kotlin/matt/obs/hold/hold.kt
+++ src/jvmMain/kotlin/matt/obs/hold/hold.kt
@@ -2,25 +2,102 @@
 
 package matt.obs.hold
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.InternalSerializationApi
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder.Companion.DECODE_DONE
+import kotlinx.serialization.encoding.CompositeDecoder.Companion.UNKNOWN_NAME
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.decodeStructure
+import kotlinx.serialization.encoding.encodeStructure
 import kotlinx.serialization.serializer
-import matt.obs.json.sers.JsonObjectFXSerializer
+import matt.lang.err
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 
-@OptIn(InternalSerializationApi::class)
+@OptIn(InternalSerializationApi::class, ExperimentalSerializationApi::class)
 open class TypedObsHolderSerializer<T: TypedObservableHolder>(
   private val cls: KClass<T>
-): JsonObjectFXSerializer<T>(cls) {
-  override fun deserialize(jsonObject: JsonObject): T {
-	return cls.createInstance().apply {
-	  namedObservables().forEach { (k, v) ->
-		jsonObject[k]?.let { /*null should be JsonNull here, so nullables should be ok*/
+): KSerializer<T> {
+
+
+  /*
+	override fun deserialize(jsonObject: JsonObject): T {
+	  return cls.createInstance().apply {
+		namedObservables().forEach { (k, v) ->
+		  jsonObject[k]?.let { */
+  /*null should be JsonNull here, so nullables should be ok*//*
+
 		  v::value.set(Json.decodeFromJsonElement(v.cls.serializer(), it))
 		}
 	  }
 	}
   }
+*/
+
+  private val exampleInstance by lazy {
+	cls.createInstance()
+  }
+
+  override val descriptor: SerialDescriptor by lazy {
+	buildClassSerialDescriptor(cls.qualifiedName!!) {
+	  exampleInstance.namedObservables().forEach {
+		element(
+		  elementName = it.key,
+		  descriptor = it.value.cls.serializer().descriptor,
+		  isOptional = true
+		)
+	  }
+	}
+  }
+
+  override fun deserialize(decoder: Decoder): T {
+	return cls.createInstance().apply {
+
+	  decoder.decodeStructure(descriptor) {
+		val observables = namedObservables()
+		while (true) {
+		  val index = decodeElementIndex(descriptor)
+		  when {
+			index >= 0            -> {
+			  val key = decodeStringElement(String::class.serializer().descriptor, index = index)
+			  val prop = observables[key]!!
+			  prop.decode(this, index = index + 1)
+			}
+
+			index == DECODE_DONE  -> break /*Input is over*/
+			index == UNKNOWN_NAME -> err("unknown name?")
+			else                  -> error("Unexpected index: $index")
+		  }
+		}
+
+	  }
+
+
+	  /*
+			namedObservables().forEach { (k, v) ->
+			  jsonObject[k]?.let { *//*null should be JsonNull here, so nullables should be ok*//*
+		  v::value.set(Json.decodeFromJsonElement(v.cls.serializer(), it))
+		}
+	  }*/
+
+
+	}
+  }
+
+  override fun serialize(encoder: Encoder, value: T) {
+	encoder.encodeStructure(descriptor) {
+	  var i = 0
+	  value.namedObservables().entries.forEach { it ->
+		encodeStringElement(descriptor = String::class.serializer().descriptor, index = i++, value = it.key)
+		it.value.encode(this, index = i++)
+	  }
+	}
+
+  }
+
+
 }
