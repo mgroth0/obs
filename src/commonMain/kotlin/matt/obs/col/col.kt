7816358acca36d13d5b1de5134f03cb4376ab811
mgroth0 <mgroth49@gmail.com>
2023-04-27 16:45:32 -0400

    changed 2 files

--- src/commonMain/kotlin/matt/obs/col/col.kt
+++ src/commonMain/kotlin/matt/obs/col/col.kt
@@ -22,85 +22,88 @@ import matt.obs.prop.ValProp
 import matt.obs.prop.VarProp
 
 
-interface BasicOCollection<E, C: CollectionChange<E, out Collection<E>>, U: CollectionUpdate<E, C>, L: CollectionListenerBase<E, C, U>>:
-	Collection<E>,
-	MListenable<L> {
-  override fun observe(op: ()->Unit) = onChange { op() }
+interface BasicOCollection<E, C : CollectionChange<E, out Collection<E>>, U : CollectionUpdate<E, C>, L : CollectionListenerBase<E, C, U>> :
+    Collection<E>,
+    MListenable<L> {
+    override fun observe(op: () -> Unit) = onChange { op() }
 
-  override fun observeWeakly(w: MyWeakRef<*>, op: ()->Unit) = onChangeWithAlreadyWeak(w) { _, _ ->
-	op()
-  }
+    override fun observeWeakly(w: MyWeakRef<*>, op: () -> Unit) = onChangeWithAlreadyWeak(w) { _, _ ->
+        op()
+    }
 
-  fun onChange(listenerName: String? = null, op: (C)->Unit): MyListenerInter<*>
-  fun <W: Any> onChangeWithWeak(o: W, op: (W, C)->Unit): MyListenerInter<*>
-  fun <W: Any> onChangeWithAlreadyWeak(weakRef: MyWeakRef<W>, op: (W, C)->Unit): MyListenerInter<*>
+    fun onChange(listenerName: String? = null, op: (C) -> Unit): MyListenerInter<*>
+    fun <W : Any> onChangeWithWeak(o: W, op: (W, C) -> Unit): MyListenerInter<*>
+    fun <W : Any> onChangeWithAlreadyWeak(weakRef: MyWeakRef<W>, op: (W, C) -> Unit): MyListenerInter<*>
 
+    
 
 }
 
 
 typealias IBObsCol = InternallyBackedOCollection<*, *, *, *>
 
-abstract class InternallyBackedOCollection<E, C: CollectionChange<E, out Collection<E>>, U: CollectionUpdate<E, C>, L: CollectionListenerBase<E, C, U>>:
-	MObservableImpl<U, L>(),
-	BasicOCollection<E, C, U, L> {
+abstract class InternallyBackedOCollection<E, C : CollectionChange<E, out Collection<E>>, U : CollectionUpdate<E, C>, L : CollectionListenerBase<E, C, U>> :
+    MObservableImpl<U, L>(),
+    BasicOCollection<E, C, U, L> {
 
 
-  override fun onChange(listenerName: String?, op: (C)->Unit): L {
-	val l = createListener {
-	  //	  println("addListener c = $it")
-	  op(it)
-	}
-	return addListener(l.also {
-	  if (listenerName != null) it.name = listenerName
-	})
-  }
+    override fun onChange(listenerName: String?, op: (C) -> Unit): L {
+        val l = createListener {
+            //	  println("addListener c = $it")
+            op(it)
+        }
+        return addListener(l.also {
+            if (listenerName != null) it.name = listenerName
+        })
+    }
 
-  protected abstract fun createListener(invoke: CollectionListener<E, C, U>.(change: C)->Unit): L
+    protected abstract fun createListener(invoke: CollectionListener<E, C, U>.(change: C) -> Unit): L
 
-  internal val bindWritePass = KeyPass()
+    internal val bindWritePass = KeyPass()
 
-  protected fun emitChange(change: C) {
-	require(this !is BindableList<*> || !this.isBound || bindWritePass.isHeld)
-	notifyListeners(updateFrom((change)))
-  }
+    protected fun emitChange(change: C) {
+        require(this !is BindableList<*> || !this.isBound || bindWritePass.isHeld)
+        notifyListeners(updateFrom((change)))
+    }
 
-  protected abstract fun updateFrom(c: C): U
+    protected abstract fun updateFrom(c: C): U
 
-  val isEmptyProp: ValProp<Boolean> by lazy {
-	VarProp(this.isEmpty()).apply {
-	  onChange {
-		value = this@InternallyBackedOCollection.isEmpty()
-	  }
-	}
-  }
+    val isEmptyProp: ValProp<Boolean> by lazy {
+        VarProp(this.isEmpty()).apply {
+            onChange {
+                value = this@InternallyBackedOCollection.isEmpty()
+            }
+        }
+    }
 }
 
-abstract class InternallyBackedOSet<E> internal constructor(): InternallyBackedOCollection<E, SetChange<E>, SetUpdate<E>, SetListenerBase<E>>() {
+abstract class InternallyBackedOSet<E> internal constructor() :
+    InternallyBackedOCollection<E, SetChange<E>, SetUpdate<E>, SetListenerBase<E>>() {
 
-  override fun updateFrom(c: SetChange<E>): SetUpdate<E> {
-	return SetUpdate(c)
-  }
+    override fun updateFrom(c: SetChange<E>): SetUpdate<E> {
+        return SetUpdate(c)
+    }
 
-  override fun createListener(invoke: CollectionListener<E, SetChange<E>, SetUpdate<E>>.(change: SetChange<E>)->Unit): SetListenerBase<E> {
-	val l = SetListener<E>(invoke)
-	return l
-  }
+    override fun createListener(invoke: CollectionListener<E, SetChange<E>, SetUpdate<E>>.(change: SetChange<E>) -> Unit): SetListenerBase<E> {
+        val l = SetListener<E>(invoke)
+        return l
+    }
 }
 
-abstract class InternallyBackedOList<E> internal constructor(): InternallyBackedOCollection<E, ListChange<E>, ListUpdate<E>, ListListenerBase<E>>() {
-  override fun updateFrom(c: ListChange<E>): ListUpdate<E> {
-	return ListUpdate(c)
-  }
+abstract class InternallyBackedOList<E> internal constructor() :
+    InternallyBackedOCollection<E, ListChange<E>, ListUpdate<E>, ListListenerBase<E>>() {
+    override fun updateFrom(c: ListChange<E>): ListUpdate<E> {
+        return ListUpdate(c)
+    }
 
-  override fun createListener(invoke: CollectionListener<E, ListChange<E>, ListUpdate<E>>.(change: ListChange<E>)->Unit): ListListenerBase<E> {
-	val l = ListListener<E>(invoke)
-	return l
-  }
+    override fun createListener(invoke: CollectionListener<E, ListChange<E>, ListUpdate<E>>.(change: ListChange<E>) -> Unit): ListListenerBase<E> {
+        val l = ListListener<E>(invoke)
+        return l
+    }
 }
 
-interface BasicOMutableCollection<E, C: CollectionChange<E, out Collection<E>>, U: CollectionUpdate<E, C>>:
-	BasicOCollection<E, C, U, CollectionListenerBase<E, C, U>>,
-	MutableCollection<E>
+interface BasicOMutableCollection<E, C : CollectionChange<E, out Collection<E>>, U : CollectionUpdate<E, C>> :
+    BasicOCollection<E, C, U, CollectionListenerBase<E, C, U>>,
+    MutableCollection<E>
 
 
--- src/commonMain/kotlin/matt/obs/col/olist/mappedlist/MappedList.kt
+++ src/commonMain/kotlin/matt/obs/col/olist/mappedlist/MappedList.kt
@@ -4,6 +4,7 @@ package matt.obs.col.olist.mappedlist
 import matt.lang.model.value.LazyValue
 import matt.lang.setall.setAll
 import matt.lang.weak.MyWeakRef
+import matt.log.taball
 import matt.obs.col.change.mirror
 import matt.obs.col.olist.BasicObservableListImpl
 import matt.obs.col.olist.ImmutableObsList
@@ -12,142 +13,148 @@ import matt.obs.col.olist.basicMutableObservableListOf
 import matt.obs.col.olist.dynamic.CalculatedList
 import matt.obs.col.olist.view
 
-fun <S, T> ImmutableObsList<S>.toMappedList(mapFun: (S)->T): MappedList<T> {
-  return BasicMappedList(
-	source = this, target = BasicObservableListImpl(), converter = mapFun
-  )
+fun <S, T> ImmutableObsList<S>.toMappedList(mapFun: (S) -> T): MappedList<T> {
+    return BasicMappedList(
+        source = this, target = BasicObservableListImpl(), converter = mapFun
+    )
 }
 
-interface MappedList<T>: CalculatedList<T> {
+interface MappedList<T> : CalculatedList<T> {
 
 }
 
 class BasicMappedList<S, T>(
-  private val source: ImmutableObsList<S>, private val target: MutableObsList<T>, private val converter: (S)->T
-): ImmutableObsList<T> by target, MappedList<T> {
+    private val source: ImmutableObsList<S>, private val target: MutableObsList<T>, private val converter: (S) -> T
+) : ImmutableObsList<T> by target, MappedList<T> {
 
-  override fun refresh() {
-	target.setAll(source.map(converter))
-  }
+    override fun refresh() {
+        target.setAll(source.map(converter))
+    }
 
-  init {
-	refresh()
-	source.onChange {
-	  target.mirror(it, converter)
-	}
-  }
+    init {
+        refresh()
+        source.onChange {
+            target.mirror(it, converter)
+        }
+    }
 
 }
 
 
-fun <W: Any, S, T> ImmutableObsList<S>.toWeakMappedList(w: W, mapFun: (W, S)->T): CalculatedList<T> {
-  return WeakMappedList(
-	weakObj = w,
-	source = this,
-	target = basicMutableObservableListOf(),
-	converter = mapFun
-  )
+fun <W : Any, S, T> ImmutableObsList<S>.toWeakMappedList(w: W, mapFun: (W, S) -> T): CalculatedList<T> {
+    return WeakMappedList(
+        weakObj = w,
+        source = this,
+        target = basicMutableObservableListOf(),
+        converter = mapFun
+    )
 }
 
 
-class WeakMappedList<W: Any, S, T>(
-  weakObj: W,
-  private val source: ImmutableObsList<S>,
-  private val target: MutableObsList<T>,
-  private val converter: (W, S)->T
-): ImmutableObsList<T> by target, MappedList<T> {
-  private val weakRef = MyWeakRef(weakObj)
+class WeakMappedList<W : Any, S, T>(
+    weakObj: W,
+    private val source: ImmutableObsList<S>,
+    private val target: MutableObsList<T>,
+    private val converter: (W, S) -> T
+) : ImmutableObsList<T> by target, MappedList<T> {
+    private val weakRef = MyWeakRef(weakObj)
 
-  override fun refresh() {
-	target.atomicChange {
-	  setAll(this@WeakMappedList.source.map {
-		this@WeakMappedList.converter(this@WeakMappedList.weakRef.deref()!!, it)
-	  })
-	}
-  }
+    override fun refresh() {
+        target.atomicChange {
+            setAll(
+                this@WeakMappedList.source.map {
+                    this@WeakMappedList.converter(this@WeakMappedList.weakRef.deref()!!, it)
+                }
+            )
+        }
+    }
 
-  init {
-	refresh()
-	source.onChangeWithAlreadyWeak(weakRef) { w, it ->
-	  target.mirror(it) {
-		converter(w, it)
-	  }
-	}
-  }
+    init {
+        refresh()
+        source.onChangeWithAlreadyWeak(weakRef) { w, it ->
+            println("source changed")
+            taball("source", source)
+            taball("target", target)
+            println("change=${it}")
+            target.mirror(it) {
+                converter(w, it)
+            }
+        }
+    }
 }
 
 
-fun <S, T> ImmutableObsList<S>.toLazyMappedList(mapFun: (S)->T): MappedList<T> {
-  return LazyMappedList(
-	source = this, target = BasicObservableListImpl(), converter = mapFun
-  )
+fun <S, T> ImmutableObsList<S>.toLazyMappedList(mapFun: (S) -> T): MappedList<T> {
+    return LazyMappedList(
+        source = this, target = BasicObservableListImpl(), converter = mapFun
+    )
 }
 
 class LazyMappedList<S, T>(
-  private val source: ImmutableObsList<S>,
-  private val target: MutableObsList<LazyValue<T>>,
-  private val converter: (S)->T
-): ImmutableObsList<T> by target.view({ it.value }), MappedList<T> {
+    private val source: ImmutableObsList<S>,
+    private val target: MutableObsList<LazyValue<T>>,
+    private val converter: (S) -> T
+) : ImmutableObsList<T> by target.view({ it.value }), MappedList<T> {
 
-  override fun refresh() {
-	target.setAll(source.map {
-	  LazyValue { converter(it) }
-	})
-  }
+    override fun refresh() {
+        target.setAll(source.map {
+            LazyValue { converter(it) }
+        })
+    }
 
 
-  init {
-	refresh()
-	source.onChange {
-	  target.mirror(it.convert(
-		target
-	  ) {
-		LazyValue {
-		  converter(it)
-		}
-	  })
-	}
-  }
+    init {
+        refresh()
+        source.onChange {
+            target.mirror(it.convert(
+                target
+            ) {
+                LazyValue {
+                    converter(it)
+                }
+            })
+        }
+    }
 }
 
 
-fun <S, T, W: Any> ImmutableObsList<S>.toLazyMappedListWithWeak(w: W, mapFun: (W, S)->T): MappedList<T> {
-  return LazyWeakMappedList(
-	weakObject = w,
-	source = this,
-	target = basicMutableObservableListOf(),
-	converter = mapFun
-  )
+fun <S, T, W : Any> ImmutableObsList<S>.toLazyMappedListWithWeak(w: W, mapFun: (W, S) -> T): MappedList<T> {
+    return LazyWeakMappedList(
+        weakObject = w,
+        source = this,
+        target = basicMutableObservableListOf(),
+        converter = mapFun
+    )
 }
 
-class LazyWeakMappedList<W: Any, S, T>(
-  weakObject: W,
-  private val source: ImmutableObsList<S>,
-  private val target: MutableObsList<LazyValue<T>> = basicMutableObservableListOf<LazyValue<T>>(),
-  private val converter: (W, S)->T,
-): ImmutableObsList<T> by target.view({ it.value }), MappedList<T> {
+class LazyWeakMappedList<W : Any, S, T>(
+    weakObject: W,
+    private val source: ImmutableObsList<S>,
+    private val target: MutableObsList<LazyValue<T>> = basicMutableObservableListOf<LazyValue<T>>(),
+    private val converter: (W, S) -> T,
+) : ImmutableObsList<T> by target.view({ it.value }), MappedList<T> {
 
-  private val weakRef = MyWeakRef(weakObject)
+    private val weakRef = MyWeakRef(weakObject)
 
-  override fun refresh() {
-	target.setAll(source.map {
-	  LazyValue {
-		converter(weakRef.deref()!!, it)
-	  }
-	})
-  }
+    override fun refresh() {
+        target.setAll(source.map {
+            LazyValue {
+                converter(weakRef.deref()!!, it)
+            }
+        })
+    }
 
-  init {
-	refresh()
-	source.onChangeWithAlreadyWeak(weakRef) { deRefed, it ->
-	  target.mirror(it.convert(
-		target
-	  ) {
-		LazyValue {
-		  converter(deRefed, it)
-		}
-	  })
-	}
-  }
+    init {
+        refresh()
+        source.onChangeWithAlreadyWeak(weakRef) { deRefed, it ->
+            target.mirror(it.convert(
+                target
+            ) {
+                LazyValue {
+                    converter(deRefed, it)
+                }
+            })
+        }
+    }
 }
 
