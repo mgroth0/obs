1c325c2ba723da26215fb11441e7ced7db6aed5c
mgroth0 <mgroth49@gmail.com>
2023-06-08 21:30:17 -0400

    changed src/commonMain/kotlin/matt/obs/col/olist/dynamic/dynamic.kt

--- src/commonMain/kotlin/matt/obs/col/olist/dynamic/dynamic.kt
+++ src/commonMain/kotlin/matt/obs/col/olist/dynamic/dynamic.kt
@@ -1,7 +1,7 @@
 package matt.obs.col.olist.dynamic
 
 import matt.collect.list.setAllOneByOneNeverAllowingDuplicates
-import matt.collect.map.lazyMap
+import matt.lang.model.value.Value
 import matt.lang.tostring.mehToStringBuilder
 import matt.lang.weak.MyWeakRef
 import matt.model.op.debug.DebugLogger
@@ -12,123 +12,152 @@ import matt.obs.col.olist.MutableObsList
 import matt.obs.col.olist.basicMutableObservableListOf
 import matt.obs.invalid.CustomDependencies
 import matt.obs.invalid.DependencyHelper
+import matt.obs.listen.MyListenerInter
 import matt.obs.prop.BindableProperty
 import matt.obs.prop.ObsVal
 
-interface CalculatedList<E>: ImmutableObsList<E> {
-  fun refresh()
+interface CalculatedList<E> : ImmutableObsList<E> {
+    fun refresh()
 }
 
-interface BasicFilteredList<E>: CalculatedList<E>, CustomDependencies, List<E> {
-  val predicate: BindableProperty<((E)->Boolean)?>
+interface BasicFilteredList<E> : CalculatedList<E>, CustomDependencies, List<E> {
+    val predicate: BindableProperty<((E) -> Boolean)?>
 }
 
-interface BasicSortedList<E>: CalculatedList<E>, CustomDependencies, List<E> {
-  val comparator: BindableProperty<Comparator<in E>?>
+interface BasicSortedList<E> : CalculatedList<E>, CustomDependencies, List<E> {
+    val comparator: BindableProperty<Comparator<in E>?>
 }
 
 class DynamicList<E>(
-  private val source: ImmutableObsList<E>,
-  filter: ((E)->Boolean)? = null,
-  private val dynamicFilter: ((E)->ObsB)? = null,
-  comparator: Comparator<in E>? = null,
-  private val target: MutableObsList<E> = basicMutableObservableListOf()
-): ImmutableObsList<E> by target, BasicFilteredList<E>, BasicSortedList<E>, CustomDependencies {
+    private val source: ImmutableObsList<E>,
+    filter: ((E) -> Boolean)? = null,
+    private val dynamicFilter: ((E) -> ObsB)? = null,
+    comparator: Comparator<in E>? = null,
+    private val target: MutableObsList<E> = basicMutableObservableListOf()
+) : ImmutableObsList<E> by target, BasicFilteredList<E>, BasicSortedList<E>, CustomDependencies {
 
 
-  override fun toString() = mehToStringBuilder("name" to nam)
+    override fun toString() = mehToStringBuilder("name" to nam)
 
-  override val predicate = BindableProperty(filter)
-  override val comparator = BindableProperty(comparator)
-
-  /*TODO: this probably needs weak refrences and other fancy techniques or its likely going to be a memory/cpu leak in multiple ways*/
-  private val dynamicPredicates = dynamicFilter?.let { filtOp ->
-	lazyMap<E, ObsB> {
-	  filtOp(it).apply {
-		observe {
-		  if (it in this@DynamicList.source) this@DynamicList.refresh()
-		}
-	  }
-	}
-  }
-
-  override fun refresh() {
-	require(predicate.value == null || dynamicFilter == null)
-	target.atomicChange {
-	  //	  println("starting atomic change of ${this@DynamicList}")
+    override val predicate = BindableProperty(filter)
+    override val comparator = BindableProperty(comparator)
 
 
-	  /*THERE MUST NEVER BE DUPLICATES GOING TO NODE LISTS
-	  *
-	  * 1. NOT ONLY DOES JAVAFX THROW A BUG FOR ADDING DUPLICATE NODES
-	  * 2. BUT ALSO, IF YOU "MOVE" A NODE BY ADDING IT SOMEWHERE THEN REMOVING IT SOMEWHERE ELSE, THE CODE COULD INTERPRET THE FINAL ACTION AS IT BEING REMOVED
-	  *
-	  * */
-	  setAllOneByOneNeverAllowingDuplicates(
-		this@DynamicList.source
-		  .filter {
-			this@DynamicList.predicate.value?.invoke(it)
-			?: this@DynamicList.dynamicPredicates?.get(it)?.value ?: true
-		  }
-		  .let {
-			val c = this@DynamicList.comparator.value
-			if (c != null) sortedWith(c)
-			else it
-		  }
-	  )
-	  //	  println("finishing atomic change of ${this@DynamicList}")
-	}
-  }
+    /*needs so much work to avoid CPU and memory issues*/
+    /*small issue: full refresh is excessive*/
+    /*big issue: are more weak references needed?*/
+    private val dynamicPredicates = dynamicFilter?.let { _ ->
+        require(filter == null)
+        mutableMapOf<E, ObsB>()
+    }
+
+    private fun newDynamicPredicate(element: E): ObsB {
+        require(predicate.value == null)
+        val bProp = dynamicFilter!!(element)
+        return bProp.apply {
+            var listener: MyListenerInter<*>? = null
+            listener = onChangeWithWeak(Value(element)) { weakElement, _ ->
+                val e = weakElement.value
+                if (e in this@DynamicList.source) {
+                    this@DynamicList.refresh()
+                } else {
+                    listener!!.removeListener()
+                    this@DynamicList.dynamicPredicates!!.remove(e)
+                }
+            }
+        }
+    }
+
+//    private val cachedPredValues = WeakMap<E, Boolean>()
+
+    override fun refresh() {
+        val predValue = predicate.value
+        require(predValue == null || dynamicFilter == null)
+        val pred: (E) -> Boolean = when {
+            predValue != null -> {
+                {
+                    predValue(it)
+                }
+            }
+
+            dynamicFilter != null -> {
+                {
+                    dynamicPredicates!!.getOrPut(it) { newDynamicPredicate(it) }.value
+                }
+            }
+
+            else -> {
+                { true }
+            }
+        }
+        /*when nodes move in node children lists in javafx, it must be atomic*/
+        target.atomicChange {
+            setAllOneByOneNeverAllowingDuplicates(
+                this@DynamicList.source
+                    .filter(pred)
+                    .let {
+                        val c = this@DynamicList.comparator.value
+                        if (c != null) sortedWith(c)
+                        else it
+                    }
+            )
+        }
+    }
 
 
-  init {
-	refresh()
-	this.predicate.observe { refresh() }
-	this.comparator.observe { refresh() }
-	source.observe { refresh() }.apply {
-	  name = "listener for ${this@DynamicList}"
-	}
-  }
+    init {
+        refresh()
+        this.predicate.observe { refresh() }
+        this.comparator.observe { refresh() }
+        source.observe { refresh() }.apply {
+            name = "listener for ${this@DynamicList}"
+        }
+    }
 
-  override fun markInvalid() {
-	refresh()
-  }
+    override fun markInvalid() {
+        refresh()
+    }
 
-  private val dependencyHelper by lazy { DependencyHelper(this) }
+    private val dependencyHelper by lazy { DependencyHelper(this) }
 
-  override fun <O: MObservable> addDependency(
-	mainDep: O,
-	moreDeps: List<MObservable>?,
-	debugLogger: DebugLogger?,
-	vararg deepDependencies: (O)->MObservable?
-  ) =
-	dependencyHelper.addDependency(mainDep = mainDep, moreDeps = moreDeps, debugLogger = debugLogger, *deepDependencies)
+    override fun <O : MObservable> addDependency(
+        mainDep: O,
+        moreDeps: List<MObservable>?,
+        debugLogger: DebugLogger?,
+        vararg deepDependencies: (O) -> MObservable?
+    ) =
+        dependencyHelper.addDependency(
+            mainDep = mainDep,
+            moreDeps = moreDeps,
+            debugLogger = debugLogger,
+            *deepDependencies
+        )
 
-  override fun <O: MObservable> addDependencyWithDeepList(o: O, deepDependencies: (O)->List<MObservable>) =
-	dependencyHelper.addDependencyWithDeepList(o, deepDependencies)
+    override fun <O : MObservable> addDependencyWithDeepList(o: O, deepDependencies: (O) -> List<MObservable>) =
+        dependencyHelper.addDependencyWithDeepList(o, deepDependencies)
 
-  override fun <O: ObsVal<*>> addDependencyIgnoringFutureNullOuterChanges(
-	o: O,
-	vararg deepDependencies: (O)->MObservable?
-  ) {
-	return dependencyHelper.addDependencyIgnoringFutureNullOuterChanges(o, *deepDependencies)
-  }
+    override fun <O : ObsVal<*>> addDependencyIgnoringFutureNullOuterChanges(
+        o: O,
+        vararg deepDependencies: (O) -> MObservable?
+    ) {
+        return dependencyHelper.addDependencyIgnoringFutureNullOuterChanges(o, *deepDependencies)
+    }
 
-  override fun removeDependency(o: MObservable) = dependencyHelper.removeDependency(o)
+    override fun removeDependency(o: MObservable) = dependencyHelper.removeDependency(o)
 
-  override fun <O: MObservable> addWeakDependency(
-	weakRef: MyWeakRef<*>,
-	mainDep: O,
-	moreDeps: List<MObservable>?,
-	debugLogger: DebugLogger?,
-	vararg deepDependencies: (O)->MObservable?
-  ) = dependencyHelper.addWeakDependency(
-	weakRef = weakRef,
-	mainDep = mainDep,
-	moreDeps = moreDeps,
-	debugLogger = debugLogger,
-	*deepDependencies
-  )
+    override fun <O : MObservable> addWeakDependency(
+        weakRef: MyWeakRef<*>,
+        mainDep: O,
+        moreDeps: List<MObservable>?,
+        debugLogger: DebugLogger?,
+        vararg deepDependencies: (O) -> MObservable?
+    ) = dependencyHelper.addWeakDependency(
+        weakRef = weakRef,
+        mainDep = mainDep,
+        moreDeps = moreDeps,
+        debugLogger = debugLogger,
+        *deepDependencies
+    )
 
 
 }
